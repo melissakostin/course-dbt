00bbc5b84fa332cb3b2dbb20cef130a515190976
Melissa Kostin <melissa.kostin@slalom.com>
2023-05-07 23:44:10 +0000

    Add timestamp to int model

--- greenery/models/intermediate/int_session_events_agg.sql
+++ greenery/models/intermediate/int_session_events_agg.sql
@@ -13,10 +13,11 @@
 select
     event_user_guid
     , event_session_guid
+    , event_created_at
     , event_order_guid
     , event_product_guid
     {%- for event_type in event_types %}
         , sum(case when event_type = '{{ event_type }}' then 1 else 0 end) as {{ event_type }}s 
     {%- endfor %}
 from {{ ref ('stg_postgres_events') }}
-group by 1,2,3,4
+group by 1,2,3,4,5
